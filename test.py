36069f9e87b468269c13aff83abfd8cea8177789
Khaled ElMorshedy <elmoiv@yahoo.com>
2022-03-21 20:24:49 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from myu_connector import MyuDaemon
+
+USERNAME = '123456789'
+PASSWORD = 'ABCDEFGHI'
+
+myu = MyuDaemon(USERNAME, PASSWORD)
+print('>>> Conencting to Myu...')
+myu.connect()
+print('>>> Openning Moodle...')
+myu.open_moodle()
