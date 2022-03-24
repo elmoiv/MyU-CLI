0b4594b4e032230a4fab4f6d16d31da1ffb418a8
Khaled ElMorshedy <elmoiv@yahoo.com>
2022-03-24 12:36:16 +0200

    Add files via upload

--- tests/open-moodle.py
+++ tests/open-moodle.py
@@ -2,10 +2,8 @@ import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.myu import Myu
 
-USERNAME = '1111111'
-PASSWORD = '1111111'
+myu = Myu(json_path='credentials.json')
 
-myu = Myu(USERNAME, PASSWORD)
 print('>>> Conencting to Myu...')
 myu.connect()
 
